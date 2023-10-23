9b49ed197f65476880f5d6731d2ff560ae1e7b4b
Tapish Verma <tapishverma176@gmail.com>
2023-10-23 22:16:45 +0530

    Import necessary libraries and do basic initial setup.

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const port = 8000
+const express = require("express")
+const path = require("path")
+const router = require("./routes/index") 
+const expressLayouts = require("express-ejs-layouts")
+
+
+const app = express()
+
+app.set("view engine","ejs")
+app.set("views" , path.join(__dirname,"views"));
+app.use(expressLayouts);
+app.use(express.static("statics"));
+app.use(express.urlencoded({extended:true})) //body parser
+
+app.use("/",router);
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error Occurred in starting server : ${err}`)
+    }else{
+        console.log(`Server started successfully at port ${port}`)
+    }
+})
